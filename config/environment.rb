d082db1b9e92aaa3f829afa1a6122a1540394a2c
QuenMBar <Qbeast360@gmail.com>
2021-04-12 14:31:11 -0700

    commit tree d0a524943bea4f81d915a378b1fc75162c20a960

--- config/environment.rb
+++ config/environment.rb
@@ -1,20 +1,16 @@
 require 'bundler/setup'
 Bundler.require
 
-Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
-Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| require f}
+Dir[File.join(File.dirname(__FILE__), '../app/models', '*.rb')].each { |f| require f }
+Dir[File.join(File.dirname(__FILE__), '../lib/support', '*.rb')].each { |f| require f }
 
-ENV["SCHOOL_ENV"] ||= "development"
+ENV['SCHOOL_ENV'] ||= 'development'
 
-DBRegistry[ENV["SCHOOL_ENV"]].connect!
+DBRegistry[ENV['SCHOOL_ENV']].connect!
 DB = ActiveRecord::Base.connection
 
-if ENV["SCHOOL_ENV"] == "test"
-  ActiveRecord::Migration.verbose = false
-end
+ActiveRecord::Migration.verbose = false if ENV['SCHOOL_ENV'] == 'test'
 
 def drop_db
-  DB.tables.each do |table|
-    DB.execute("DROP TABLE #{table}")
-  end
+    DB.tables.each { |table| DB.execute("DROP TABLE #{table}") }
 end
